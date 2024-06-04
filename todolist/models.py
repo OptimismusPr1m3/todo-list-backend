b672468779a92f01b172d5546bd5ec2bd5eafed0
Sebastian Wolff <bastiwolff432@gmail.com>
2024-06-04 17:56:52 +0200

    continued todolist training with backend and frontend

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
+import datetime
 
-# Create your models here.
+class TodoItem(models.Model):
+    created_at = models.DateField(default=datetime.date.today)
+    title = models.CharField(max_length=100, blank=True, default='')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    checked = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'  ({self.id})  {self.title}'
--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-zrdg^_o!4at5*b(qc(n+p5vc9&azxq0_=e4r=qe7!mjn84-al5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
+
+CORS_ALLOWED_ORIGINS = [ 'http://localhost:4200', ]
 
 
 # Application definition
@@ -38,10 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'todolist'
+    'todolist',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
